f73146c8addc114098190f745014b477cdf25d24
Jordan B. Sanders <jordan@SandStormIT.com>
2019-08-02 17:15:42 -0500

    Reorders song request url validation
    This change changes the order of URLs that we check to increase the
    chances that we check fewer statements.

--- app/commands/song_request.rb
+++ app/commands/song_request.rb
@@ -11,14 +11,14 @@ module Synthia::Command
     end
 
     def self.valid_url?(url)
-      return true if url[0,20] == 'https://youtube.com/'
-      return true if url[0,24] == 'https://www.youtube.com/'
-      return true if url[0,19] == 'http://youtube.com/'
-      return true if url[0,23] == 'http://www.youtube.com/'
-      return true if url[0,17] == 'https://youtu.be/'
       return true if url[0,21] == 'https://www.youtu.be/'
-      return true if url[0,16] == 'http://youtu.be/'
+      return true if url[0,24] == 'https://www.youtube.com/'
+      return true if url[0,23] == 'http://www.youtube.com/'
       return true if url[0,20] == 'http://www.youtu.be/'
+      return true if url[0,17] == 'https://youtu.be/'
+      return true if url[0,20] == 'https://youtube.com/'
+      return true if url[0,19] == 'http://youtube.com/'
+      return true if url[0,16] == 'http://youtu.be/'
       false
     end
   end
